e2de830c347b90366def517a7d7ebfb2c2c50370
anhsbolic <anhsbolic@gmail.com>
2018-02-13 07:27:32 +0700

    Get data from firebase then set to UI

--- app/src/main/java/id/bantu/bantuputu/activity/MainActivity.java
+++ app/src/main/java/id/bantu/bantuputu/activity/MainActivity.java
@@ -6,10 +6,19 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 import id.bantu.bantuputu.R;
+import id.bantu.bantuputu.adapter.MyImagesAdapter;
 import id.bantu.bantuputu.model.MyImage;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         initComponent();
         setRecyclerView();
+
+        loadDataThenSetToRvMyImages();
     }
 
     private void initComponent() {
@@ -41,5 +52,57 @@ public class MainActivity extends AppCompatActivity {
         rvMyImages.setLayoutManager(lmRvMyImages);
         rvMyImages.setItemAnimator(itemAnimatorRvMyImages);
         rvMyImages.addItemDecoration(dividerRvMyImages);
+        adapterRvMyImages = new MyImagesAdapter(MainActivity.this, dataImages,
+                new MyImagesAdapter.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(MyImage myImage) {
+                        String strId = myImage.getIdMyImage();
+                        String strName = myImage.getName();
+                        String strUrl = myImage.getUrl();
+                        String strToast = strId + "-" + strName + "-" + strUrl;
+                        Toast.makeText(MainActivity.this, strToast, Toast.LENGTH_LONG).show();
+                    }
+                });
+        rvMyImages.setAdapter(adapterRvMyImages);
+    }
+
+    private void loadDataThenSetToRvMyImages() {
+        DatabaseReference myImagesDbReff = FirebaseDatabase.getInstance().getReference()
+                .child("my-images");
+
+        myImagesDbReff.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if(dataSnapshot.exists()){
+                    MyImage myImageData = dataSnapshot.getValue(MyImage.class);
+                    if(myImageData != null){
+                        myImageData.setIdMyImage(dataSnapshot.getKey());
+                        dataImages.add(myImageData);
+                        adapterRvMyImages.notifyItemChanged(dataImages.size()-1);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
